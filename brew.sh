8e3d46a5d2ff963bfc99f101cd6c8e8d197c07a0
Jonathan Moss <jwmoss88+github@gmail.com>
2020-11-21 18:42:36 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -63,4 +63,6 @@ brew cleanup
 ## Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+## Install PowerLevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
