8d936ee3ee621008d0ae17ffbbb6546762b0886c
Jonathan Moss <jwmoss88+github@gmail.com>
2020-11-21 18:38:44 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -36,10 +36,27 @@ brew install ${PACKAGES[@]}
 CASKAPPS=(
     firefox
     powershell
+    dropbox
+    visual-studio-code
+    discord
 )
+
+# Install Apps from App Store
+mas install 808809998 ## Reeder 3
+mas install 1384080005 ## Tweetbot 3
+mas install 409203825 ## Numbers
+mas install 409201541 ## Pages
+mas install 409183694 ## Keynote
+mas install 803453959 ## Slack
+mas install 525912054 ## Wifi Signal
+mas install 784801555 ## OneNote
+
 
 # Install all the cask apps
 brew cask install ${CASKAPPS[@]}
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+## Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
